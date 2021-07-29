b27285d8808571b6dfbe76d5d36870f075ddcfcc
Yarchick333 <yarchick.balaban@gmail.com>
2021-07-29 12:04:13 +0300

    Do't add cities to database and no sity.name in front page for home age of site

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,4 +1,33 @@
+import requests
 from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+
 
 def index(request):
-    return render(request, 'weather/index.html')
+    appid = 'ec0d578652d3d13271ad50884e3b8ac5'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res["main"]["temp"],
+            'icon': res["weather"][0]["icon"]
+        }
+
+        all_cities.append(city_info)
+
+    context = {'all_info': all_cities, 'form': form}
+
+    return render(request, 'weather/index.html', context)
